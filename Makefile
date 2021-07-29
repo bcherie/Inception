d3f0ec54afd5c3583c0d69efa971c2289c9fdff7
bcherie <bcherie@student.21-school.ru>
2021-07-29 03:21:02 +0300

    gggg

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 all:
 	docker-compose -f srcs/docker-compose.yaml up --build -d
-clean:
+vclean:
 	rm -rf /home/bcherie/data/*
 logs:
 	docker-compose logs
 re:
 	clean all
+
+clean:
+	docker stop $$(docker ps -a -q)
+	docker container prune
+	docker image prune -a
--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 mv /wordpress/* /var/www/wordpress
 mv wp-config-sample.php /var/www/wordpress
+
+# mv wp-cli.phar /usr/local/bin/wp
+# wp core config --dbhost=${WORDPRESS_DB_HOST} --dbname=${MYSQL_WP_NAME} --dbuser=${MYSQL_USER_NAME} --dbpass=${MYSQL_USER_PASSWORD} --allow-root
+# wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_N} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+wp core config --dbhost=${WORDPRESS_DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
+wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_N} --admin_password=${WP_ADMIN_PW}  --allow-root
+
+wp user create --allow-root second second@42.fr --role=author --user_pass=1111
+# mv * /var/www/wordpress/
+
 /usr/sbin/php-fpm7.3 --nodaemonize
 service php7.3-fpm start
 /usr/bin/php7.3
